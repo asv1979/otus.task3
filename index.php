304a1418fa049351b0f6f05d712f4ee91615c9c2
Alexey Vasilyev <asv2108@gmail.com>
2021-09-01 19:06:20 +0300

    init

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require 'vendor/autoload.php';
+
+use Asv1979\Otus3\Test;
+
+$test = new Test();
+var_dump($test->run());
---
+++ src/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Asv1979\Otus3;
+
+class Test
+{
+    /**
+     * @return string
+     */
+    public function run():string
+    {
+        return json_encode('Hello world!');
+    }
+}
